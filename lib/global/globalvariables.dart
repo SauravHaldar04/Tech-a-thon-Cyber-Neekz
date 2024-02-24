29331d9751ad6d2ebc7a02006b8e016e8b3f8483
Saurav Haldar <143389228+SauravHaldar04@users.noreply.github.com>
2024-02-24 12:50:35 +0530

    Adding initial page UI

---
+++ lib/global/globalvariables.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class GlobalVariables {
+  static const Gradient primaryGradient = LinearGradient(colors: [
+    Color.fromRGBO(255, 155, 99, 1),
+    Color.fromRGBO(255, 98, 31, 1)
+  ]);
+}
